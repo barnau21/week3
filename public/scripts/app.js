caad0d3eff9a5fea7014b8fbed8acb629462b764
Adrian Dumitriu2 <adrian2@Adrians-MacBook-Air.local>
2021-09-27 15:53:46 +0300

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+// iife - immediately invoked function expression
+(function(){
+
+    function Start(){
+        console.log("app started...");
+    }
+
+    window.addEventListener("load", Start);
+})();
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/About', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET Products page. */
+router.get('/Projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact Us page. */
+router.get('/Contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
+module.exports = router;
